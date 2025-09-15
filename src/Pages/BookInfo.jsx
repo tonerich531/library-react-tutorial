d1828e484649c34d78d45d59ff35c89dd9b8b29e
tonerich531 <tonerich531@yahoo.com>
2025-09-15 09:47:43 -0700

    fifteenth push

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -9,7 +9,7 @@ import Book from "../components/ui/Book";
 
 
 const BookInfo = ({ books, addToCart, cart }) => {
-    const { id } = useParams
+    const { id } = useParams()
     const book = books.find(book => +book.id === +id)
         
 
--- src/components/ui/Rating.jsx
+++ src/components/ui/Rating.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Rating = ({ rating }) => {
     return (
-        <div className="book__rating">            
+        <div className="book__ratings">            
             {new Array(Math.floor(rating)).fill(0).map((_, index) => (
                 <FontAwesomeIcon icon= "star" key={index} />
             ))}
